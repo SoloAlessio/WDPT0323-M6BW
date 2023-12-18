8ff96ad519f67db8ba5c6369ce9695fb4ba62c29
EriksVon <erica.ropelato@gmail.com>
2023-12-18 20:40:36 +0100

    Update userRouter.js

--- src/users/userRouter.js
+++ src/users/userRouter.js
@@ -3,7 +3,7 @@ const userRouter = express.Router();
 import { User } from "./models/users.js";
 
 userRouter.get("/test", async (req, res) => {
-  const user = await User.findById("655bb0def47dc0bbbd21a6b4");
+  const user = await User.findById("...");
   res.json(user);
 });
 
