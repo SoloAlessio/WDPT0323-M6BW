a68371d60c390396d6e5125f1c6e888d93495f2b
EriksVon <erica.ropelato@gmail.com>
2023-12-18 20:56:30 +0100

    .

--- src/users/userRouter.js
+++ src/users/userRouter.js
@@ -2,9 +2,21 @@ import express from "express";
 const userRouter = express.Router();
 import { User } from "./models/users.js";
 
-userRouter.get("/test", async (req, res) => {
-  const user = await User.findById("...");
-  res.json(user);
-});
+userRouter
+  .get("/test", async (req, res) => {
+    const user = await User.findById("...");
+    res.json(user);
+  })
 
+  .patch("/:id/avatar", upload.single("avatar"), async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await User.findById(id);
+      user.avatar = req.file.path;
+      const updatedUser = await user.save();
+      res.json(updatedUser);
+    } catch (error) {
+      next(error);
+    }
+  });
 export default userRouter;
